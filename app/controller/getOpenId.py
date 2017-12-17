7623bd48908b86db83bb92f53aa3f5093e5b7442
miko <625099302@qq.com>
2017-12-17 17:44:52 +0800

    增加登录

---
+++ app/controller/getOpenId.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask import request
+import json
+from urllib import request as urlRequest
+from util.dbTool import *
+app = Flask(__name__)
+
+
+@app.route('/getOpenId')
+def getOpenId():
+    jscode = request.values.get('code')
+    APPID = 'wxa4123ad325278cdf'
+    SECRET = '125a6cbbd99f928c6b0d98406842560c'
+    url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + APPID + '&secret=' + SECRET + '&js_code='\
+        + jscode + '&grant_type=authorization_code'
+
+
+    response = urlRequest.urlopen(url) 
+    page = response.read()
+    page = json.loads(page.decode('utf-8'))
+    print(page)
+    res = dict()
+    res['status'] = 1
+    res['data'] = page
+    return json.dumps(res)
+
+
+@app.route('/login')
+def login():
+    id = request.values.get("id")
+    name = request.values.get("nickName")
+    sex = request.values.get("gender")
+    address = request.values.get("city")
+    img_url = request.values.get("avatarUrl")
+
+    
+    conn = mysql_conn()
+    data = (id, name, sex, address, img_url)
+    #查询用户是否存在
+    sql_select = "select * from user where id=%s"
+    where = (id, )
+    status = mysql_sel(conn, sql_select, where)
+    if not status:
+        # 记录用户信息
+        sql_insert = "insert into user (id, name, sex, address, img_url) values( %s, %s, %s, %s, %s)"
+        
+        status = mysql_ins(conn, sql_insert, data)
+    res = dict()
+    res['status'] = status
+    res['data'] = data
+    
+    return json.dumps(res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
