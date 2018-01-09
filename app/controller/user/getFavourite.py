ba5492c796ac228f24edf043c933c14dd34a46c5
chenajiajia <924193453@qq.com>
2018-01-09 22:49:23 +0800

    666

--- app/controller/user/getFavourite.py
+++ app/controller/user/getFavourite.py
@@ -4,14 +4,14 @@ from . import user
 import json
 
 
-@user.route('/getFavourite', methods=['GET'])
+@user.route('/getFavourite', methods=['POST'])
 def getFavourite():
     # 获取post中的参数
-    # data = request.get_data().decode('utf8')
-    # json_data = json.loads(data)
-    # id = json_data['id']
-    args = request.args.to_dict()
-    id = args.get("id", "0")
+    data = request.get_data().decode('utf8')
+    json_data = json.loads(data)
+    id = json_data['id']
+    # args = request.args.to_dict()
+    # id = args.get("id", "0")
 
     # 检查参数并设置返回状态码status和信息message
     status = 1
