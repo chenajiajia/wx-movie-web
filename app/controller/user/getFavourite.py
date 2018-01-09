269dcc2b9cab156a0aea505ae337a3fbffca974c
chenajiajia <924193453@qq.com>
2018-01-09 22:37:44 +0800

    更新偏好接口

--- app/controller/user/getFavourite.py
+++ app/controller/user/getFavourite.py
@@ -4,12 +4,14 @@ from . import user
 import json
 
 
-@user.route('/getFavourite', methods=['POST'])
+@user.route('/getFavourite', methods=['GET'])
 def getFavourite():
     # 获取post中的参数
-    data = request.get_data().decode('utf8')
-    json_data = json.loads(data)
-    id = json_data['id']
+    # data = request.get_data().decode('utf8')
+    # json_data = json.loads(data)
+    # id = json_data['id']
+    args = request.args.to_dict()
+    id = args.get("id", "0")
 
     # 检查参数并设置返回状态码status和信息message
     status = 1
@@ -25,9 +27,11 @@ def getFavourite():
         result = mysql_sel(conn, sql, param)
         mysql_close(conn)
         if len(result) == 0:
-            result = ""
+            result_data = ""
+        else :
+            result_data = result[0][0]
 
-    temp_json = {"status": status, "message": message, "data":result[0][0]}
+    temp_json = {"status": status, "message": message, "data":result_data}
     result_json = json.dumps(temp_json)
     return result_json
 
